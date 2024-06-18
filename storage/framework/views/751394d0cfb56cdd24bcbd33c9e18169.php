181619e151a9761166aec5d2657cc29ca7612947
Eldar7661 <eldar7661@gmail.com>
2024-06-18 17:59:52 +0500

    not gitignore

---
+++ storage/framework/views/751394d0cfb56cdd24bcbd33c9e18169.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!-- <html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>"> -->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>IS 201 Aliev Eldar</title>
+
+        <!-- <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?> -->
+        <?php echo app('Illuminate\Foundation\Vite')(['resources/sass/app.scss', 'resources/js/app.js']); ?>
+
+    </head>
+    <body id="app"></body>
+</html>
+<?php /**PATH /home/eldar/coding/php/projects_laravel/is201/resources/views/app.blade.php ENDPATH**/ ?>
